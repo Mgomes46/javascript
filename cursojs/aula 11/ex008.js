6b479a5139ea28dad98a7562e235b2116f9e5d22
Moises Gomes <mooisesgom@gmail.com>
2023-06-08 11:40:29 -0300

    ex008

---
+++ cursojs/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel  = 78.2
+if (vel > 60) { //Condição simples!
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+}
+console.log(`Vocé ultrapassou a velocidade permitida. MULTADO!`)
+console.log(`Dirija sempre usando cinto de segurança!`)
