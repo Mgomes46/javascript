abeeb36719d77be32af3f4ce3e2115fbab24ef84
Moises Gomes <mooisesgom@gmail.com>
2023-06-27 07:07:48 -0300

    atualizei

--- cursojs/aula15/aula16ex/ex016/script.js
+++ cursojs/aula15/aula16ex/ex016/script.js
@@ -31,6 +31,29 @@ function adicionar() {
     num.value = ''
     num.focus()
 }
+
+function finalizar() {
+    if (valores.length == 0) {
+        Window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let midia = 0 
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+    }
+}
         
 
         
